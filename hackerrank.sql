2b512530cbd38bc99654ce1b87ad392e8135ae54
mrnguyener21 <victortnguyen.workemail@gmail.com>
2021-06-16 21:20:31 -0700

    completed challenge THE PADS

--- hackerrank.sql
+++ hackerrank.sql
@@ -682,3 +682,8 @@ where rownum = 1;
 select name ||'('||substr(occupation,1,1)||')' 
 from occupations
 order by name;
+
+select 'There are a total of ' || count(*) || ' ' || lower(occupation) || 's.'
+from occupations
+group by occupation
+order by count(*), occupation;
